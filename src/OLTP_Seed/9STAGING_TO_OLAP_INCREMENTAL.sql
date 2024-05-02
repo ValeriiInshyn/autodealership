452975fbd8051b17d027a22936af9a5d9877acdd
Roman Yatskovyna <roman.yatskovyna@intenselab.com>
2024-05-02 01:38:15 +0100

    incremental

--- src/OLTP_Seed/9STAGING_TO_OLAP_INCREMENTAL.sql
+++ src/OLTP_Seed/9STAGING_TO_OLAP_INCREMENTAL.sql
@@ -224,103 +224,106 @@ IF @tempcount<>0
     END
 
 --Sales
--- ;WITH SalesData AS (
---     SELECT 
---         dc.DealershipId AS DealershipId,
---         c.BrandId,
---         FORMAT(cs.SaleDate, 'yyyy-MM') as SaleDate,
---         SUM(c.Price) AS TotalPrice,
---         COUNT(c.Price) as SalesCount
---     FROM
---         AutoDealershipStaging.dbo.CarSales cs 
---         JOIN AutoDealershipStaging.dbo.DealershipCars dc ON cs.DealershipCarId = dc.Id
---         JOIN AutoDealershipStaging.dbo.Cars c ON dc.CarId = c.Id
---     GROUP BY
---         dc.DealershipId,
---         c.BrandId,
---         FORMAT(cs.SaleDate, 'yyyy-MM')
--- ),
+;WITH SalesData AS (
+    SELECT 
+        dc.DealershipId AS DealershipId,
+        c.BrandId,
+        FORMAT(cs.SaleDate, 'yyyy-MM') as SaleDate,
+        SUM(c.Price) AS TotalPrice,
+        COUNT(c.Price) as SalesCount
+    FROM
+        AutoDealershipStaging.dbo.CarSales cs 
+        JOIN AutoDealershipStaging.dbo.DealershipCars dc ON cs.DealershipCarId = dc.Id
+        JOIN AutoDealershipStaging.dbo.Cars c ON dc.CarId = c.Id
+    GROUP BY
+        dc.DealershipId,
+        c.BrandId,
+        FORMAT(cs.SaleDate, 'yyyy-MM')
+),
 
--- RankedSalesData AS (
---     SELECT
---         DealershipId,
---         BrandId,
---         CONVERT(DATE, SaleDate + '-01') AS SaleDate,
---         TotalPrice as TotalIncomeForCurrentMonth,
---         ISNULL(LAG(TotalPrice, 1) OVER (PARTITION BY DealershipId, BrandId ORDER BY SaleDate), 0) AS TotalIncomeLastMonth,
---         ISNULL(LAG(SalesCount, 1) OVER (PARTITION BY DealershipId, BrandId ORDER BY SaleDate), 0) AS SalesCountForLastMonth,
---         SalesCount AS SalesCountForCurrentMonth
---     FROM
---         SalesData
--- )
--- merge AutoDealershipOLAP.dbo.CarSalesFact as trg
--- using 
--- 	(SELECT
---     DealershipId,
---     BrandId,
---     TotalIncomeLastMonth,
---     (SELECT TOP 1 id FROM AutoDealershipOLAP.dbo.DatesDim d WHERE d.Year=YEAR(SaleDate) AND d.Month=MONTH(SaleDate) AND d.[Day]=1) AS StartDateId,
---     (SELECT TOP 1 id FROM AutoDealershipOLAP.dbo.DatesDim d WHERE d.Year=YEAR(SaleDate) AND d.Month=MONTH(SaleDate) AND d.[Day]=DAY(EOMONTH(SaleDate))) AS EndDateId,
---     TotalIncomeForCurrentMonth,
---     (TotalIncomeForCurrentMonth - TotalIncomeLastMonth) / ((TotalIncomeForCurrentMonth + TotalIncomeLastMonth) /2 ) * 100 - 100 AS MonthTotalIncomeModifyPercent,
---     SalesCountForLastMonth,
---     SalesCountForCurrentMonth,
---     SalesCountForCurrentMonth - SalesCountForLastMonth AS SalesCountChangeForMonth,
---     SaleDate
--- FROM
---     RankedSalesData
--- GROUP BY
---     DealershipId,
---     BrandId,
---     SaleDate,
---     TotalIncomeLastMonth,
---     TotalIncomeForCurrentMonth,
---     SalesCountForCurrentMonth,
---     SalesCountForLastMonth) as src
+RankedSalesData AS (
+    SELECT
+        DealershipId,
+        BrandId,
+        CONVERT(DATE, SaleDate + '-01') AS SaleDate,
+        TotalPrice as TotalIncomeForCurrentMonth,
+        ISNULL(LAG(TotalPrice, 1) OVER (PARTITION BY DealershipId, BrandId ORDER BY SaleDate), 0) AS TotalIncomeLastMonth,
+        ISNULL(LAG(SalesCount, 1) OVER (PARTITION BY DealershipId, BrandId ORDER BY SaleDate), 0) AS SalesCountForLastMonth,
+        SalesCount AS SalesCountForCurrentMonth
+    FROM
+        SalesData
+        --Filter by saleDAte
+        WHERE SaleDate >= DATEADD(MONTH, DATEDIFF(MONTH, 0, @lastloaddate), 0)
+)
+merge AutoDealershipOLAP.dbo.CarSalesFact as trg
+using 
+	(SELECT
+    DealershipId,
+    BrandId,
+    TotalIncomeLastMonth,
+    (SELECT TOP 1 id FROM AutoDealershipOLAP.dbo.DatesDim d WHERE d.Year=YEAR(SaleDate) AND d.Month=MONTH(SaleDate) AND d.[Day]=1) AS StartDateId,
+    (SELECT TOP 1 id FROM AutoDealershipOLAP.dbo.DatesDim d WHERE d.Year=YEAR(SaleDate) AND d.Month=MONTH(SaleDate) AND d.[Day]=DAY(EOMONTH(SaleDate))) AS EndDateId,
+    TotalIncomeForCurrentMonth,
+    (TotalIncomeForCurrentMonth - TotalIncomeLastMonth) / ((TotalIncomeForCurrentMonth + TotalIncomeLastMonth) /2 ) * 100 - 100 AS MonthTotalIncomeModifyPercent,
+    SalesCountForLastMonth,
+    SalesCountForCurrentMonth,
+    SalesCountForCurrentMonth - SalesCountForLastMonth AS SalesCountChangeForMonth,
+    SaleDate
+FROM
+    RankedSalesData
+GROUP BY
+    DealershipId,
+    BrandId,
+    SaleDate,
+    TotalIncomeLastMonth,
+    TotalIncomeForCurrentMonth,
+    SalesCountForCurrentMonth,
+    SalesCountForLastMonth
+) as src
+on
+	(YEAR(src.SaleDate)=YEAR(@lastloaddate) AND MONTH(src.SaleDate)=MONTH(@lastloaddate))
+when matched then
+	update set 
+	AutoDealershipId = src.DealershipId,
+    BrandId=src.BrandId,
+    TotalIncomeLastMonth=src.TotalIncomeLastMonth,
+    StartDateId=src.StartDateId,
+    EndDateId=src.EndDateId,
+    TotalIncomeForCurrentMonth=src.TotalIncomeForCurrentMonth,
+    MonthTotalIncomeModifyPercent=src.MonthTotalIncomeModifyPercent,
+    SalesCountForLastMonth=src.SalesCountForLastMonth,
+    SalesCountForCurrentMonth=src.SalesCountForCurrentMonth,
+    SalesCountChangeForMonth=src.SalesCountChangeForMonth
+when not matched then
+	insert(AutoDealershipId,
+    BrandId,
+    TotalIncomeLastMonth,
+    StartDateId,
+    EndDateId,
+    TotalIncomeForCurrentMonth,
+    MonthTotalIncomeModifyPercent,
+    SalesCountForLastMonth,
+    SalesCountForCurrentMonth,
+    SalesCountChangeForMonth)
+	values(src.DealershipId,
+    src.BrandId,
+    src.TotalIncomeLastMonth,
+    src.StartDateId,
+    src.EndDateId,
+    src.TotalIncomeForCurrentMonth,
+    src.MonthTotalIncomeModifyPercent,
+    src.SalesCountForLastMonth,
+    src.SalesCountForCurrentMonth,
+    src.SalesCountChangeForMonth);
 
--- on
--- 	(YEAR(src.SaleDate)=YEAR(@lastloaddate) AND MONTH(src.SaleDate)=MONTH(@lastloaddate))
--- when matched then
--- 	update set 
--- 	AutoDealershipId = src.DealershipId,
---     BrandId=src.BrandId,
---     TotalIncomeLastMonth=src.TotalIncomeLastMonth,
---     StartDateId=src.StartDateId,
---     EndDateId=src.EndDateId,
---     TotalIncomeForCurrentMonth=src.TotalIncomeForCurrentMonth,
---     MonthTotalIncomeModifyPercent=src.MonthTotalIncomeModifyPercent,
---     SalesCountForLastMonth=src.SalesCountForLastMonth,
---     SalesCountForCurrentMonth=src.SalesCountForCurrentMonth,
---     SalesCountChangeForMonth=src.SalesCountChangeForMonth
--- when not matched then
--- 	insert( BrandId,
---     TotalIncomeLastMonth,
---     StartDateId,
---     EndDateId,
---     TotalIncomeForCurrentMonth,
---     MonthTotalIncomeModifyPercent,
---     SalesCountForLastMonth,
---     SalesCountForCurrentMonth,
---     SalesCountChangeForMonth)
--- 	values(src.DealershipId,
---     src.BrandId,
---     src.TotalIncomeLastMonth,
---     src.StartDateId,
---     src.EndDateId,
---     src.TotalIncomeForCurrentMonth,
---     src.MonthTotalIncomeModifyPercent,
---     src.SalesCountForLastMonth,
---     src.SalesCountForCurrentMonth,
---     src.SalesCountChangeForMonth);
-
--- SET @tempcount = @@ROWCOUNT
--- IF @tempcount<>0
---     BEGIN
---         SET @rowcount = @rowcount+@tempcount;
---         SET @dwtablecount = @dwtablecount+1;
---         SET @dbtablecount = @dbtablecount+3;
---         SET @metastring = @metastring + 'insert into AutoDealershipMetadata.dbo.dw_table_data_load_history(dw_table_id,data_load_history_id) values(3,' + CONVERT(varchar(MAX),@ident) + ');'
---     END
+SET @tempcount = @@ROWCOUNT
+IF @tempcount<>0
+    BEGIN
+        SET @rowcount = @rowcount+@tempcount;
+        SET @dwtablecount = @dwtablecount+1;
+        SET @dbtablecount = @dbtablecount+3;
+        SET @metastring = @metastring + 'insert into AutoDealershipMetadata.dbo.dw_table_data_load_history(dw_table_id,data_load_history_id) values(3,' + CONVERT(varchar(MAX),@ident) + ');'
+    END
 
 --Load to Meta DB
 insert into AutoDealershipMetadata.dbo.data_load_history(load_datetime,load_time,load_rows,affected_table_count,source_table_count)
